12c8840636828c294245bb760410aba1a4d468cd
С - 14 < - 14@DESKTOP-O8LQCCI>
2023-04-28 21:13:55 +0500

    Добавьте файлы проекта.

---
+++ DelegateExample/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    delegate void output(params object[] values);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            output myPrint = (object[] _arr) => 
+            {
+                foreach (var item in _arr)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+            myPrint += (object[] _arr) =>
+            {
+                using (var sw = new StreamWriter("output.txt"))
+                {
+                    foreach (var item in _arr)
+                    {
+                        sw.WriteLine(item.ToString(), true);
+                    }
+                    
+                }
+            };
+            myPrint(1, "два", 3);
+        }
+    }
+}
---
+++ Lesson7_CS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegateExample
+{
+    delegate void output(params object[] values);
+    internal class Program
+    {
+        static void NamesOfObj(object[] _arr)
+        {
+            string output = string.Empty; //задали пустую строку, можно ""
+            foreach (var item in _arr)
+            {
+                output += $"{item.GetType()}\n";
+            }
+            MessageBox.Show(output);
+        }
+        static void Main(string[] args)
+        {
+            output myPrint = (object[] _arr) =>
+            {
+                foreach (var item in _arr)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+            myPrint += NamesOfObj;
+            myPrint += (object[] _arr) =>
+            {
+                using (var sw = new StreamWriter("output.txt", true))
+                {
+                    foreach (var item in _arr)
+                    {
+                        sw.WriteLine(item.ToString(), true);
+                    }
+                }
+            };
+            myPrint(1, "два", 3.5F, 'F');
+            myPrint -= NamesOfObj;
+            myPrint(1, 2d, (byte)25, true);
+        }
+    }
+}
